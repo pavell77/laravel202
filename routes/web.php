7084a0789e1f5074b38ef0209b16922e50bc8fa0
pavell77 <lupandinpavel77@gmail.com>
2024-12-28 18:55:09 +0000

    add first test routes1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello, World!'; // Простое текстовое сообщение
+});
+
+Route::get('/users/{id}', function ($id) {
+    return 'User ID: ' . $id; // Параметр в маршруте
+});
