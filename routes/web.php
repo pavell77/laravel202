c56f94a9264c24d46385a2b966c821b3ceff9138
pavell77 <lupandinpavel77@gmail.com>
2024-12-28 19:26:55 +0000

    rout  home1

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+#Route::get('/', function () {
+#    return view('welcome');
+#});
+
+Route::get('/', [HomeController::class, 'index'])->name('home'); // Маршрут к методу index контроллера HomeController, с именем 'home'
 
 Route::get('/hello', function () {
     return 'Hello, World!'; // Простое текстовое сообщение
