e7faa75c185012e70897a2b0baf358570de61bfa
pavell77 <lupandinpavel77@gmail.com>
2024-12-29 12:51:17 +0000

    edit welcome.blade 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,11 @@
         @endif
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+    @extends('layouts.app')
+
+    @section('content')
+        <h1>Welcome!</h1>
+    @endsection
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
             <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" alt="Laravel background" />
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
@@ -58,11 +63,7 @@
                         @endif
                     </header>
 
-                    @extends('layouts.app')
-
-                    @section('content')
-                        <h1>Welcome!</h1>
-                    @endsection
+                    
 
                     <main class="mt-6">
                         <div class="grid gap-6 lg:grid-cols-2 lg:gap-8">
