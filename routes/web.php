9fd7c31688bd042955be29e7888b283fdb844150
pavell77 <lupandinpavel77@gmail.com>
2024-12-29 13:01:07 +0000

    edit web.php

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController; // <- ДОБАВЬТЕ ЭТУ СТРОКУ!
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('home'); // Маршрут home
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [RegisterController::class, 'register'])->name('register.submit');
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout'); // Используем POST для logout
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
