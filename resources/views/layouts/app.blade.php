fbbbdf5e2ac0954350b1a3ddabcb9e9fc5cf534b
pavell77 <lupandinpavel77@gmail.com>
2024-12-29 12:54:38 +0000

    logout a to form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,10 @@
     <nav>
         <a href="{{ route('home') }}">Home</a>
         @auth
-            <a href="{{ route('logout') }}">Logout</a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+        </form>
+        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
         @else
             <a href="{{ route('login') }}">Login</a>
             <a href="{{ route('register') }}">Register</a>
