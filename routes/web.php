2515efdfe90e5772582813a4a09e72a9d7c53c70
pavell77 <lupandinpavel77@gmail.com>
2024-12-29 12:29:43 +0000

    add HomeController

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RegisterController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [HomeController::class, 'index'])->name('home'); // Маршрут home
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-Route::post('/register', [RegisterController::class, 'register']);
-
-
-#Route::get('/', [HomeController::class, 'index'])->name('home'); // Маршрут к методу index контроллера HomeController, с именем 'home'
-
-#Route::get('/hello', function () {
-#    return 'Hello, World!'; // Простое текстовое сообщение
-#});
-
-#Route::get('/users/{id}', function ($id) {
-#    return 'User ID: ' . $id; // Параметр в маршруте
-#});
+Route::post('/register', [RegisterController::class, 'register'])->name('register.submit');
